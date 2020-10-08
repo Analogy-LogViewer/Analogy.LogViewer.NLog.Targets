6b8713140c8766aa2ce548adf47503f82d2b96aa
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-10-08 08:45:50 +0300

    disable empty test

--- Analogy.LogViewer.NLog.Targets.UnitTests/UnitTest1.cs
+++ Analogy.LogViewer.NLog.Targets.UnitTests/UnitTest1.cs
@@ -1,13 +1,13 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Analogy.LogViewer.NLog.Targets.UnitTests
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-    }
-}
+//namespace Analogy.LogViewer.NLog.Targets.UnitTests
+//{
+//    [TestClass]
+//    public class UnitTest1
+//    {
+//        [TestMethod]
+//        public void TestMethod1()
+//        {
+//        }
+//    }
+//}
